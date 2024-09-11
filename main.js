e84bfcfd98a6b3f01aca135705d8514da1a3c1f4
Farhan Usmani <32909839+IamFarhanUsmani@users.noreply.github.com>
2024-09-11 18:32:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
         clickable: true,
     },
-    effect: "cube",   // Slide effect (can use 'fade' or 'cube', swipe)
+    effect: "swipe",   // Slide effect (can use 'fade' or 'cube', swipe)
     keyboard: {
         enabled: true,
 }
