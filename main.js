59dc53682d723823d4019fd118e99f6ea778aca1
Farhan Usmani <32909839+IamFarhanUsmani@users.noreply.github.com>
2024-12-21 06:38:45 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const swiper = new Swiper('.swiper-container', {
     centeredSlides: true,     // Always center the active slide
     spaceBetween: 30,         // Space between slides
     //loop: true,               // Loop through slides
-    initialSlide: 20,  // Center latest Episode
+    initialSlide: 21,  // Center latest Episode
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
